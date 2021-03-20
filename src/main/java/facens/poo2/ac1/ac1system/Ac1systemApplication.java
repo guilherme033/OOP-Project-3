0877ae2ed07079513b2eaa9ed69633e2d2453301
Guilherme <guipereira1586@outlook.com>
2021-03-20 18:59:46 -0300

    First Commit

---
+++ src/main/java/facens/poo2/ac1/ac1system/Ac1systemApplication.java
@@ -0,0 +1,13 @@
+package facens.poo2.ac1.ac1system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac1systemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac1systemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/facens/poo2/ac1/ac1system/Ac1systemApplicationTests.java
@@ -0,0 +1,13 @@
+package facens.poo2.ac1.ac1system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ac1systemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
