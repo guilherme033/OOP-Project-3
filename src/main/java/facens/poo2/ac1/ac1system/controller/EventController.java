a0fd7f4b90810cfbc66b808e9d8c8014d0e89a9f
Guilherme <guipereira1586@outlook.com>
2021-03-20 23:57:06 -0300

    Eighth commit

--- src/main/java/facens/poo2/ac1/ac1system/controller/EventController.java
+++ src/main/java/facens/poo2/ac1/ac1system/controller/EventController.java
@@ -3,19 +3,20 @@ package facens.poo2.ac1.ac1system.controller;
 import java.net.URI;
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import facens.poo2.ac1.ac1system.dto.EventDTO;
 import facens.poo2.ac1.ac1system.dto.EventInsertDTO;
+import facens.poo2.ac1.ac1system.dto.EventUpdateDTO;
 import facens.poo2.ac1.ac1system.service.EventService;
 
 @RestController
@@ -43,4 +44,17 @@ public class EventController {
             URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
             return ResponseEntity.created(uri).body(dto);
     }
+
+    @DeleteMapping("{id}")
+    public  ResponseEntity <Void> delete(@PathVariable Long id){
+            service.delete(id);
+            return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public  ResponseEntity <EventDTO> update(@PathVariable Long id, @RequestBody EventUpdateDTO updateDto){
+            EventDTO dto = service.update(id, updateDto);
+            return ResponseEntity.ok().body(dto);
+    }
+
 }
--- src/main/java/facens/poo2/ac1/ac1system/repositories/EventRepository.java
+++ src/main/java/facens/poo2/ac1/ac1system/repositories/EventRepository.java
@@ -1,10 +1,10 @@
 package facens.poo2.ac1.ac1system.repositories;
 
+import facens.poo2.ac1.ac1system.entities.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class EventRepository {
-
-    
+public interface EventRepository extends JpaRepository <Event, Long> {
     
 }
