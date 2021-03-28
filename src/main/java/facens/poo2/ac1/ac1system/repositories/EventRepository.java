b3330492a9b3b121d23715eb13b075c59238b79b
Guilherme <guipereira1586@outlook.com>
2021-03-27 21:56:19 -0300

    Tenth commit

--- src/main/java/facens/poo2/ac1/ac1system/repositories/EventRepository.java
+++ src/main/java/facens/poo2/ac1/ac1system/repositories/EventRepository.java
@@ -1,10 +1,24 @@
 package facens.poo2.ac1.ac1system.repositories;
-
 import facens.poo2.ac1.ac1system.entities.Event;
+import java.time.LocalDate;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends JpaRepository <Event, Long> {
-    
+
+     @Query("SELECT e FROM Event e "    + "WHERE"                                   + 
+     "( LOWER(e.name) LIKE   LOWER(CONCAT('%', :name,    '%')))  AND"               +
+     "( LOWER(e.description)   LIKE   LOWER(CONCAT('%', :description, '%'))) AND"   +
+     "( LOWER(e.place)   LIKE   LOWER(CONCAT('%', :place, '%'))) AND"               +
+     "( LOWER(e.startdate)   LIKE   LOWER(CONCAT('%', :startdate, '%')))"       
+     
+)
+
+public Page <Event> find(Pageable pageRequest, String name, String description, String place, LocalDate startedate);
+
 }
+ 
--- src/main/java/facens/poo2/ac1/ac1system/service/EventService.java
+++ src/main/java/facens/poo2/ac1/ac1system/service/EventService.java
@@ -1,17 +1,18 @@
 package facens.poo2.ac1.ac1system.service;
 
 import facens.poo2.ac1.ac1system.entities.Event;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
 import facens.poo2.ac1.ac1system.dto.EventDTO;
 import facens.poo2.ac1.ac1system.dto.EventInsertDTO;
 import facens.poo2.ac1.ac1system.dto.EventUpdateDTO;
@@ -20,58 +21,65 @@ import facens.poo2.ac1.ac1system.repositories.EventRepository;
 @Service
 public class EventService {
 
-    @Autowired
-    private EventRepository repository;
+@Autowired
+private EventRepository repository;
 
-    public List<EventDTO> getEvents() {
-        List<Event> list = repository.findAll();
-            return toDTOList(list);
-    }
-
-    public EventDTO geteventbyid(Long id) {
-        Optional<Event> op = repository.findById(id);
-            Event ev = op.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Client not found"));
-                return new EventDTO(ev);
-    }
+public Page<EventDTO> getEvents(PageRequest pageRequest, String name, String place, String description, LocalDate startdate){
     
-    public EventDTO insert(EventInsertDTO dto){
-        Event entity = new Event(dto);
-            entity = repository.save(entity);
-                return new EventDTO(entity);
+    Page<Event> list = repository.find(pageRequest, name, place, description, startdate);
+    return list.map( e -> new EventDTO(e));
     }
 
-    public void delete(Long id){
-        try{
-            repository.deleteById(id);
-        }
-        catch(EmptyResultDataAccessException e){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Client not found");
-        }
-    }
 
-    public EventDTO update(Long id, EventUpdateDTO dto){
-      try{
-        Event entity = repository.getOne(id);
-            entity.setName(dto.getName());
-                entity = repository.save(entity);
-                     return new EventDTO(entity);
-      }
-      catch(EntityNotFoundException ex){
+public EventDTO geteventbyid(Long id) {
+    Optional<Event> op = repository.findById(id);
+    Event ev = op.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Client not found"));
+    return new EventDTO(ev);
+}
+    
+public EventDTO insert(EventInsertDTO dto){
+    Event entity = new Event(dto);
+    entity = repository.save(entity);
+    return new EventDTO(entity);
+}
+
+public void delete(Long id){
+    try
+    {
+        repository.deleteById(id);
+    }
+    catch(EmptyResultDataAccessException e)
+    {
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Client not found");
-      }
-        
     }
+}
 
-    public List<EventDTO> toDTOList(List<Event> list) {
+public EventDTO update(Long id, EventUpdateDTO dto){
+    try
+    {
+        Event entity = repository.getOne(id);
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new EventDTO(entity);
+    }
+    catch(EntityNotFoundException ex)
+    {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Client not found");
+    }
+        
+}
 
-        List<EventDTO> listDTO = new ArrayList<>();
+public List<EventDTO> toDTOList(List<Event> list) {
 
-        for(Event e : list) {
-            EventDTO dto = new EventDTO(e.getId(), e.getName());
-                listDTO.add(dto);
-        }
+    List<EventDTO> listDTO = new ArrayList<>();
+
+    for(Event e : list) 
+    {
+        EventDTO dto = new EventDTO(e.getId(), e.getName());
+        listDTO.add(dto);
+    }
 
         return listDTO;
-    }
+}
 
 }
